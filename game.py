bdd579db2c416d0332da7649f5035a08ef03ed15
mr-omi <omksbhosale18@gmail.com>
2023-06-05 11:23:29 +0530

    updated py file

--- game.py
+++ game.py
@@ -141,3 +141,34 @@ class Board:
         string_rep = indices_row + '-' * str_len + '\n' + string_rep + '-' * str_len
 
         return string_rep
+
+
+# play the game
+def play(dim_size=10, num_bombs=10):
+    # Step 1: create the board and plant the bombs
+    board = Board(dim_size, num_bombs)
+
+    # Step 2: show the user the board and ask for where they want to dig
+    # Step 3a: if location is a bomb, show game over message
+    # Step 3b: if location is not a bomb, dig recursively until each square is at least
+    #          next to a bomb
+    # Step 4: repeat steps 2 and 3a/b until there are no more places to dig -> VICTORY!
+    safe = True
+
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        # 0,0 or 0, 0 or 0,    0
+        try:
+            x, y = map(int, (input("Where would you like to dig? Input as row,col: ").split(" ")))
+            row, col = x, y
+        except:
+            continue
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Try again.")
+            continue
+
+        # if it's valid, we dig
+        safe = board.dig(row, col)
+        if not safe:
+            # dug a bomb ahhhhhhh
+            break  # (game over rip)
